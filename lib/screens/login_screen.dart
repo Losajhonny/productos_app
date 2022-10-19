0aafe6870a80f542c95ef4ace97d04c8ee438275
Jhonatan Lopez <jlopez@develsystems.com>
2022-10-18 20:41:27 -0600

    Input correo

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -16,6 +16,7 @@ class LoginScreen extends StatelessWidget {
         children: [
           //
           SizedBox(height: 250),
+
           CardContainer(
             child: Column(
               children: [
@@ -29,12 +30,58 @@ class LoginScreen extends StatelessWidget {
                 SizedBox(
                   height: 30,
                 ),
-                Text('Formulario')
+                _LoginForm()
               ],
             ),
+          ),
+
+          SizedBox(height: 50),
+          Text(
+            'Crear una nueva cuenta',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           )
         ],
       ),
     )));
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        // TODO: mantener la referencia al key
+        // estado de referencia
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.deepPurple),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple,
+                    width: 2,
+                  ),
+                ),
+                hintText: 'losajhonny@gmail.com',
+                labelText: 'Email',
+                labelStyle: TextStyle(color: Colors.grey),
+                prefixIcon: Icon(
+                  Icons.alternate_email_sharp,
+                  color: Colors.deepPurple,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
